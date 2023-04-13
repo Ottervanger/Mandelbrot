574251bd73d21e9a4a87ef9b2814940483552e84
Gilles Ottervanger <gillesottervanger@hotmail.com>
2023-04-13 22:40:33 +0200

    Add pan and zoom persistence

--- app.js
+++ app.js
@@ -28,4 +28,18 @@ window.onload = () => {
     tileCallback[e.data.hash](null, e.data.tile);
     delete tileCallback[e.data.hash];
   }
+
+  const writeCoordsToHash = function(event) {
+    let c = event.target.viewport.getCenter();
+    let zoom = event.target.zoom;
+    window.location.hash = `${c.x}:${c.y}:${zoom}`;
+  }
+
+  window.plot.on(lumo.PAN_END, writeCoordsToHash);
+  window.plot.on(lumo.ZOOM_END, writeCoordsToHash);
+
+  if (window.location.hash) {
+    var [x, y, z] = window.location.hash.substr(1).split(':').map(parseFloat);
+    window.plot.zoomToPosition(z, {x: x, y: y}, antimate=false);
+  }
 };
